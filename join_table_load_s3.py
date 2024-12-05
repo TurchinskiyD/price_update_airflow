004f85c1ef6142e44f56776d6b1569a1cb5e658d
TurchinskiyDima <turchinskiyd@gmail.com>
2024-12-05 22:51:23 +0200

    change func fetch_data_from_db add process_data

--- join_table_load_s3.py
+++ join_table_load_s3.py
@@ -1,8 +1,9 @@
+from datetime import datetime
+
 import pandas as pd
 import boto3
 import psycopg2
 from io import BytesIO
-from sqlalchemy import create_engine
 
 import sys
 sys.path.append("..")
@@ -19,14 +20,14 @@ S3_CONFIG = {
 
 
 # Функція для отримання даних з бази даних
-def fetch_data_from_db():
+def fetch_data_from_db(db_conn_config):
     query = """
         SELECT pu.product_id, pu.status, pu.current_price, pu.product_name, ps.sale
         FROM prod_update pu
         LEFT JOIN prod_sale ps ON pu.product_id = ps.product_id;
     """
     try:
-        with psycopg2.connect(**DB_CONFIG) as conn:
+        with psycopg2.connect(**db_conn_config) as conn:
             return pd.read_sql_query(query, conn)
     except Exception as e:
         print("Error fetching data from database:", e)
@@ -34,22 +35,17 @@ def fetch_data_from_db():
 
 
 # Функція для збереження файлів у S3
-def upload_to_s3(dataframe, filename, file_format):
+def upload_to_s3(dataframe, filename, s3_conn_config):
     s3 = boto3.client(
         's3',
-        aws_access_key_id=S3_CONFIG['aws_access_key_id'],
-        aws_secret_access_key=S3_CONFIG['aws_secret_access_key']
+        aws_access_key_id=s3_conn_config['aws_access_key_id'],
+        aws_secret_access_key=s3_conn_config['aws_secret_access_key']
     )
 
     buffer = BytesIO()
 
-    if file_format == 'csv':
-        dataframe.to_csv(buffer, index=False)
-    elif file_format == 'xlsx':
-        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            dataframe.to_excel(writer, index=False, sheet_name='Sheet1')
-    else:
-        raise ValueError("Unsupported file format")
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+        dataframe.to_excel(writer, index=False, sheet_name='Sheet1')
 
     buffer.seek(0)
 
@@ -57,17 +53,31 @@ def upload_to_s3(dataframe, filename, file_format):
     print(f"Uploaded {filename} to S3")
 
 
+def process_data(df):
+    # Перейменування колонок
+    df.columns = ['Артикул', 'Наявність', 'Ціна', 'Назва', 'Знижка']
+
+    # Перетворення 'Ціна' в числовий формат
+    df['Ціна'] = pd.to_numeric(df['Ціна'], errors='coerce')
+
+    # Замінити порожні значення у 'Знижка' на 0
+    df['Знижка'] = df['Знижка'].fillna(0)
+
+    return df
+
+
+now = datetime.now()
+formatted_time = now.strftime("%Y_%m_%d_%H_%M")
+
+
 # Основна функція
 def main():
     try:
         print("Fetching data from database...")
-        df = fetch_data_from_db()
-
-        print("Uploading CSV file to S3...")
-        upload_to_s3(df, 'prod_data.csv', 'csv')
+        df = fetch_data_from_db(DB_CONFIG)
 
         print("Uploading XLSX file to S3...")
-        upload_to_s3(df, 'prod_data.xlsx', 'xlsx')
+        upload_to_s3(process_data(df), f'update_with_sale_{formatted_time}.xlsx', S3_CONFIG)
 
         print("Process completed successfully!")
     except Exception as e:
