bb04940ad28fd9f1d0f2a32f4e5e0a3acd523783
TurchinskiyDima <turchinskiyd@gmail.com>
2024-12-13 14:56:14 +0200

    change 4 tasks

--- dags/dag_price_update.py
+++ dags/dag_price_update.py
@@ -1,24 +1,23 @@
+import os
+import sys
+import json
+
+# Aбсолютний шлях до проекту
+sys.path.append('/price_update_airflow')
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from processing import main_outfit
+from processing.main_outfit import outfit_file_operation
 from for_update import download_file, add_for_update, create_price_xlsx
 from load_csv_to_db import processing_csv
 from load_sale_file import download_file_from_s3, load_excel_to_database
 from join_table_load_s3 import fetch_data_from_db, process_data, upload_to_s3
 from main import functions_list
 
-import sys
 sys.path.append("..")
 from config.config import (link_list, SQLALCHEMY_DATABASE_URI, DB_POSTGRES_CONFIG, AWS_ACCESS_KEY_ID,
                            AWS_SECRET_ACCESS_KEY)
-
-# Конфігурація
-default_args = {
-    'owner': 'airflow',
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
 
 # Дані для обробки
 csv_file_path = "price_update.csv"
@@ -31,12 +30,19 @@ S3_CONFIG = {
 }
 
 
+# Конфігурація
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
 with DAG(
     dag_id='price_update_pipeline',
     default_args=default_args,
     description='Pipeline for processing price updates',
     schedule_interval='@daily',
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2024, 12, 12),
     catchup=False,
 ) as dag:
 
@@ -56,63 +62,104 @@ with DAG(
         do_xcom_push=True  # зберігати результати в XCom
     )
 
+
     # Таск 3. Додавання результатів обробки прайсів до порівняння
     def add_update_data(**kwargs):
         # отрммуємо дані з XCom
-        data_for_update = kwargs['ti'].xcom_pull(task_ids='process_outfit')
+        ti = kwargs['ti']
+        data_for_update = ti.xcom_pull(task_ids='process_outfit')
 
+        # Перевірка, чи є отримані дані
+        if not data_for_update:
+            raise ValueError("Дані для оновлення відсутні в XCom")
+
+        # Оновлюємо дані
         for name_func, func in functions_list:
             result_dict = func()
             add_for_update(name_func, result_dict, data_for_update)
 
-    # Таск 5. Створення xlsx файлу
-    create_price_task = PythonOperator(
-        task_id='create_price_xlsx',
-        python_callable=create_price_xlsx,
-        op_kwargs={'data_for_update': main_outfit.outfit_file_operation()}
+        # Приведення типів поля 'price' перед збереженням у XCom
+        for key, item in data_for_update.items():
+            if 'price' in item:
+                # Перевіряємо, чи значення 'price' є рядком і перетворюємо його на float
+                if isinstance(item['price'], str):
+                    try:
+                        item['price'] = float(item['price'].replace(',', '.'))
+                    except ValueError:
+                        raise ValueError(f"Неправильний формат ціни для артикулу {key}: {item['price']}")
+
+            # Зберігаємо оновлені дані в XCom для наступного таска
+        ti.xcom_push(key='updated_data', value=data_for_update)
+
+
+    add_update_task = PythonOperator(
+        task_id='add_update_data',
+        python_callable=add_update_data,
+        provide_context=True  # дозволяє передати контекст XCom
     )
 
-    # Завдання 6. Обробка CSV файлу для бази даних
-    process_csv_task = PythonOperator(
-        task_id='process_csv',
-        python_callable=processing_csv,
-        op_kwargs={'file_path': csv_file_path, 'database_url': DATABASE_URL}
+    # Таск 4. Створення xlsx файлу
+    def prepare_data_for_create_xlsx(**kwargs):
+        ti = kwargs['ti']
+        # Отримуємо дані з XCom
+        raw_data = ti.xcom_pull(task_ids='add_update_data', key='updated_data')
+
+        # Перевіряємо, чи дані є рядком, і перетворюємо їх на словник
+        if isinstance(raw_data, str):
+            dict_for_update = json.loads(raw_data)
+        else:
+            dict_for_update = raw_data
+
+        # Викликаємо функцію з передачею словника
+        create_price_xlsx(dict_for_update)
+
+
+    prepare_xlsx_task = PythonOperator(
+        task_id='prepare_data_for_create_xlsx',
+        python_callable=prepare_data_for_create_xlsx,
+        provide_context=True
     )
 
-    # Завдання 7. Завантаження файлу зі знижками до DB
-    download_s3_task = PythonOperator(
-        task_id='download_sale_file',
-        python_callable=download_file_from_s3,
-        op_kwargs={
-            'bucket_name': S3_CONFIG['bucket_name'],
-            'file_name': 'sale.xlsx',
-            'local_file_path': 'sale.xlsx'
-        }
-    )
+    # # Завдання 6. Обробка CSV файлу для бази даних
+    # process_csv_task = PythonOperator(
+    #     task_id='process_csv',
+    #     python_callable=processing_csv,
+    #     op_kwargs={'file_path': csv_file_path, 'database_url': DATABASE_URL}
+    # )
+    #
+    # # Завдання 7. Завантаження файлу зі знижками до DB
+    # download_s3_task = PythonOperator(
+    #     task_id='download_sale_file',
+    #     python_callable=download_file_from_s3,
+    #     op_kwargs={
+    #         'bucket_name': S3_CONFIG['bucket_name'],
+    #         'file_name': 'sale.xlsx',
+    #         'local_file_path': 'sale.xlsx'
+    #     }
+    # )
+    #
+    # # Завдання 8. Завантаження файлу з оновленими прайсами до DB
+    # load_excel_task = PythonOperator(
+    #     task_id='load_excel_to_db',
+    #     python_callable=load_excel_to_database,
+    #     op_kwargs={'file_path': 'sale.xlsx', 'db_url': DATABASE_URL}
+    # )
+    #
+    # # Завдання 9. Обробка файлу в Data Bas
+    # process_db_task = PythonOperator(
+    #     task_id='process_db_data',
+    #     python_callable=lambda: process_data(fetch_data_from_db(DB_CONFIG))
+    # )
+    #
+    # # Завдання 10. Завантаження оновленого прайсу з доданими знижками до S3 bucket
+    # upload_s3_task = PythonOperator(
+    #     task_id='upload_clean_data_to_s3',
+    #     python_callable=upload_to_s3,
+    #     op_kwargs={
+    #         'data': process_data(fetch_data_from_db(DB_CONFIG)),
+    #         'config': S3_CONFIG
+    #     }
+    # )
 
-    # Завдання 8. Завантаження файлу з оновленими прайсами до DB
-    load_excel_task = PythonOperator(
-        task_id='load_excel_to_db',
-        python_callable=load_excel_to_database,
-        op_kwargs={'file_path': 'sale.xlsx', 'db_url': DATABASE_URL}
-    )
 
-    # Завдання 9. Обробка файлу в Data Bas
-    process_db_task = PythonOperator(
-        task_id='process_db_data',
-        python_callable=lambda: process_data(fetch_data_from_db(DB_CONFIG))
-    )
-
-    # Завдання 10. Завантаження оновленого прайсу з доданими знижками до S3 bucket
-    upload_s3_task = PythonOperator(
-        task_id='upload_clean_data_to_s3',
-        python_callable=upload_to_s3,
-        op_kwargs={
-            'data': process_data(fetch_data_from_db(DB_CONFIG)),
-            'config': S3_CONFIG
-        }
-    )
-
-
-    download_task >> outfit_task >> add_update_task >> create_price_task >> process_csv_task
-    process_csv_task >> download_s3_task >> load_excel_task >> process_db_task >> upload_s3_task
+    download_task >> outfit_task >> add_update_task >> prepare_xlsx_task
